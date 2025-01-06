8ab266391dccaffb7c56837ceaccffa2e48ed7ef
dwndkaa <muhammaddwiandhika945@gmail.com>
2025-01-07 00:54:10 +0700

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\DHIKA\\Flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\DHIKA\\PEMOGRAMAN MOBILE\\tugas7" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\DHIKA\\Flutter\\flutter"
+  "PROJECT_DIR=C:\\Users\\DHIKA\\PEMOGRAMAN MOBILE\\tugas7"
+  "FLUTTER_ROOT=C:\\Users\\DHIKA\\Flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\DHIKA\\PEMOGRAMAN MOBILE\\tugas7\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\DHIKA\\PEMOGRAMAN MOBILE\\tugas7"
+  "FLUTTER_TARGET=C:\\Users\\DHIKA\\PEMOGRAMAN MOBILE\\tugas7\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\DHIKA\\PEMOGRAMAN MOBILE\\tugas7\\.dart_tool\\package_config.json"
+)
